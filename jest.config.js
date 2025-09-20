b6f65db5f40a3e9fb2e8792bad19896e87ab68b8
Craig Savage <csavage647@gmail.com>
2025-09-19 23:01:16 -0400

    added more packages to handle browser env test and react test + added initial test

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+};
