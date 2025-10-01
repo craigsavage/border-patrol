9a5dba1c798a707c661a88d0e6e629ebc46c7a90
Craig Savage <csavage647@gmail.com>
2025-09-30 21:47:56 -0400

    fixed path alias

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ export default {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   moduleNameMapper: {
+    '^scripts/(.*)$': '<rootDir>/src/scripts/$1',
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
   moduleDirectories: ['node_modules', 'src'],
