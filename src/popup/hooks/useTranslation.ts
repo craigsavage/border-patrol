2f2c8ca045293192a7da27a3e92395167736625f
Craig Savage <csavage647@gmail.com>
2026-04-17 02:36:00 -0400

    simplified locale usage

--- src/popup/hooks/useTranslation.ts
+++ src/popup/hooks/useTranslation.ts
@@ -1,19 +1,6 @@
+import { localeMap } from '../locales';
 import enMessages from '../../_locales/en/messages.json';
-import esMessages from '../../_locales/es/messages.json';
-import deMessages from '../../_locales/de/messages.json';
-import frMessages from '../../_locales/fr/messages.json';
-import jaMessages from '../../_locales/ja/messages.json';
 import { useLocaleContext } from '../context/LocaleContext';
-import { LocaleCode, MessagesType } from '../../types/translations';
-
-// Map of locale codes to their respective message objects
-const localeMap: Record<LocaleCode, MessagesType> = {
-  en: enMessages,
-  es: esMessages,
-  fr: frMessages,
-  de: deMessages,
-  ja: jaMessages,
-};
 
 /**
  * Custom hook to provide translation functionality based on the current locale.
